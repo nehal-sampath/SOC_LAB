b9935e941b49dddb0748bcb69e4691b6a62d83dd
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-10-06 21:01:50 +0530

    Create Person.java

---
+++ SOC_lab5/Person.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SOC_ASS5_package;
+
+/**
+ *
+ * @author nehal
+ */
+public class Person {
+    
+    private String pname;
+    private String city;
+    public String getPname(){
+        return pname;
+    }
+    public void setPname(String pname){
+        this.pname = pname;
+    }
+    
+    public String getCity(){
+        return city;
+    }
+    
+    public void setCity(String city){
+        this.city=city;
+    }
+    
+}
